a7eb292516ec9de6aa3e01bcd74c6fd3871aa854
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-07-28 06:58:20 +0900

    feat: アセンブリ生成関数を追加

---
+++ src/generate.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include "./struct/node.h"
+
+//
+// Code generator
+//
+
+/* スタックマシンライクな構文木生成関数 */
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
