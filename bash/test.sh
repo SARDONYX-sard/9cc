91057ed194d9b59923a0250d2b43c324d4e0ac24
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2021-07-28 15:47:09 +0900

    test: 比較演算子のテスト追加

--- bash/test.sh
+++ bash/test.sh
@@ -22,13 +22,35 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
+
 # 乗除
 assert 47 "5+6*7"
 assert 15 "5*(9-6)"
 assert 4 "(3+5)/2"
+
 # 単項
 assert 10 "-10+20"
 assert 10 "- -10"
 assert 10 "- - +10"
+
+# 比較演算子
+assert 0 '0==1'
+assert 1 '42==42'
+assert 1 '0!=1'
+assert 0 '42!=42'
+
+assert 1 '0<1'
+assert 0 '1<1'
+assert 0 '2<1'
+assert 1 '0<=1'
+assert 1 '1<=1'
+assert 0 '2<=1'
+
+assert 1 '1>0'
+assert 0 '1>1'
+assert 0 '1>2'
+assert 1 '1>=0'
+assert 1 '1>=1'
+assert 0 '1>=2'
 
 echo OK
